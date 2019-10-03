91e66ac72a9507e769f90dd87b150c3fd09df162
Marian Pysko <maryanpysko@gmail.com>
2019-10-03 21:35:47 +0300

    socket

---
+++ app.js
@@ -0,0 +1,33 @@
+const status = document.getElementById('status');
+const messages = document.getElementById('messages');
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const date = document.getElementById('date')
+const ws = new WebSocket('ws://localhost:3000')
+
+
+function setStatus(value) {
+    status.innerHTML = value;
+}
+
+function printMessage(value) {
+    const li = document.createElement('li');
+
+    li.innerHTML = value;
+    
+    
+}
+
+
+form.addEventListener('submit', e=>{
+    e.preventDefault();
+    ws.send(input.value);
+    input.value=''
+   
+})
+
+ws.onopen = () => setStatus('ONLINE')
+
+ws.onclose = () => setStatus('DISCONNECTED')
+
+ws.onmessage = response => printMessage(response.data)
